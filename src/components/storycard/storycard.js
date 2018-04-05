235b60ca776c07d8a6e3a07668b96b02d6dff7ab
Byron Griffith <byrongriffith91@gmail.com>
2018-04-05 16:30:42 -0400

    story fixed

--- src/components/storycard/storycard.js
+++ src/components/storycard/storycard.js
@@ -4,8 +4,13 @@ import { Card, Button, CardTitle, CardText } from 'reactstrap';
 const Storycard = (props) => {
   return (  
       <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below <input type="text"></input> as a natural lead-in to additional content.</CardText>
+        <CardTitle>City Madlib</CardTitle>
+        <CardText>As you walk along the main street in (city name) you see a large 
+        (animal name). You move closer to investigate, but the animal 
+        begins to charge at you. You turn and run while shouting “(an exclamation)!” 
+        You trip and roll into a ditch where you find a/an (noun). You use it to ward 
+        off the beast and save (celebrity name) from danger. Your new celebrity friend 
+        thanks you and asks you to a dinner of (food).</CardText>
         <Button>Button</Button>
       </Card>
   );
